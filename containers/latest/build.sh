c850ff8b735609dd407a52346c1141e56d4fb975
lwmacctw <lwmacct@icloud.com>
2026-07-01 19:16:12 +0800

    chore 2026-07-01 19:16:12 CST

--- containers/latest/build.sh
+++ containers/latest/build.sh
@@ -1,18 +1,33 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-root_dir="$(git rev-parse --show-toplevel)"
-cd "$root_dir"
+__main() {
+  :
+  _root_dir="$(git rev-parse --show-toplevel)"
+  cd "$_root_dir" || return 1
 
-image="${1:-ghcr.io/lwmacct/260629-cr-nginx:latest}"
-platforms="${PLATFORMS:-linux/amd64,linux/arm64}"
+  _url=$(git remote get-url origin)
+  _path=$(printf '%s\n' "$_url" | sed -E '
+    s#^[^/:]+@[^:]+:##;
+    s#^[a-zA-Z][a-zA-Z0-9+.-]*://([^@/]+@)?[^/]+/##;
+    s#\.git$##
+  ')
 
-docker buildx build \
-  --builder "${BUILDER:-default}" \
-  --platform "$platforms" \
-  --file containers/latest/Dockerfile \
-  --tag "$image" \
-  --network host \
-  --progress plain \
-  --push \
-  .
+  _owner="${_path%/*}"
+  _repo="${_path##*/}"
+  _image="${1:-ghcr.io/${_owner}/${_repo}:latest}"
+  _platforms="${PLATFORMS:-linux/amd64,linux/arm64}"
+
+  docker buildx build \
+    --builder "${BUILDER:-default}" \
+    --platform "$_platforms" \
+    --file containers/latest/Dockerfile \
+    --tag "$_image" \
+    --network host \
+    --progress plain \
+    --push \
+    .
+
+}
+
+__main
